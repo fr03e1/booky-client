e7728776a195246be36e65abcef6f3a6f7849b92
fr03e1 <aleksejbocarov65@gmail.com>
2023-05-19 14:10:07 +0300

    test

---
+++ src/components/Categories.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <section className="product-categories-one pb-60">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-12 wow fadeInUp animated">
+                        <div className="product-categories-one__inner">
+                            <ul>
+                                <li><a href="#0" className="img-box">
+                                    <div className="inner"><img src="assets/images/shop/product-categories-v1-img1.png"
+                                                                alt=""/></div>
+                                </a>
+                                    <div className="title"><a href="#0">
+                                        <h6>Accessories</h6>
+                                    </a></div>
+                                </li>
+                                <li><a href="#0" className="img-box">
+                                    <div className="inner"><img src="assets/images/shop/product-categories-v1-img2.png"
+                                                                alt=""/></div>
+                                </a>
+                                    <div className="title"><a href="#0">
+                                        <h6>Furnitures</h6>
+                                    </a></div>
+                                </li>
+                                <li><a href="#0" className="img-box">
+                                    <div className="inner"><img src="assets/images/shop/product-categories-v1-img3.png"
+                                                                alt=""/></div>
+                                </a>
+                                    <div className="title"><a href="#0">
+                                        <h6>Jewellery</h6>
+                                    </a></div>
+                                </li>
+                                <li><a href="#0" className="img-box">
+                                    <div className="inner"><img src="assets/images/shop/product-categories-v1-img4.png"
+                                                                alt=""/></div>
+                                </a>
+                                    <div className="title"><a href="#0">
+                                        <h6>Shoes</h6>
+                                    </a></div>
+                                </li>
+                                <li><a href="#0" className="img-box">
+                                    <div className="inner"><img src="assets/images/shop/product-categories-v1-img5.png"
+                                                                alt=""/></div>
+                                </a>
+                                    <div className="title"><a href="#0">
+                                        <h6>Electronics</h6>
+                                    </a></div>
+                                </li>
+                                <li><a href="#0" className="img-box">
+                                    <div className="inner"><img src="assets/images/shop/product-categories-v1-img6.png"
+                                                                alt=""/></div>
+                                </a>
+                                    <div className="title"><a href="#0">
+                                        <h6>Fashion</h6>
+                                    </a></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Categories;
